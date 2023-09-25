5fc284591baf895b5ba9a2646665901d120922b1
Rasoul Veisi <rasoulweysi@gmail.com>
2023-09-25 09:03:35 +0330

    change name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,6 +25,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ScalesOps app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('InfiniteScroll app is running!');
   });
 });
