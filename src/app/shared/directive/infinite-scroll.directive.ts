cf6abd9219ecf3e828f0da8bc5e6a930b68af71b
Rasoul Veisi <62328821+rasoulveisi@users.noreply.github.com>
2024-01-21 16:59:14 +0330

    fix scroll down behaviour

--- src/app/shared/directive/infinite-scroll.directive.ts
+++ src/app/shared/directive/infinite-scroll.directive.ts
@@ -1,13 +1,14 @@
-import { Directive, EventEmitter, HostListener, Output } from '@angular/core';
+import { Directive, EventEmitter, HostListener, Input, Output } from "@angular/core";
 
 @Directive({
-  selector: '[infinitScroll]',
+  selector: "[infinitScroll]",
 })
 export class infinitScrollDirective {
+  @Input() threshold:number = 85;
   @Output() scrollEndReached = new EventEmitter<boolean>();
-  scrollThreshold = 95;
+  previousValues = { scrollTop: 0, scrollHeight: 0 };
 
-  @HostListener('scroll', ['$event'])
+  @HostListener("scroll", ["$event"])
   onScroll(event: Event) {
     const element = event.target as HTMLElement;
     const scrollTop = element.scrollTop;
@@ -17,7 +18,17 @@ export class infinitScrollDirective {
     const maxScrollDistance = scrollHeight - clientHeight;
     const scrollPercentage = (scrollTop / maxScrollDistance) * 100;
 
-    if (scrollPercentage >= this.scrollThreshold) {
+    const isScrollDown = scrollTop > this.previousValues.scrollTop;
+    this.previousValues.scrollTop = scrollTop;
+
+    const isHeightChanged = this.previousValues.scrollHeight < scrollHeight;
+
+    if (this.previousValues.scrollHeight > scrollHeight) {
+      this.previousValues.scrollHeight = 0;
+    }
+    
+    if (isScrollDown && isHeightChanged && scrollPercentage >= this.threshold) {
+      this.previousValues.scrollHeight = scrollHeight;
       this.scrollEndReached.emit(true);
     }
   }
